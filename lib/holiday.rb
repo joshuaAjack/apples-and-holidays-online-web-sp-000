89ae5c64d559a81f8d3998dd0a595e3ad999e2e7
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-04 23:05:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,6 @@
+require 'pry'
 
- def second_supply_for_fourth_of_july(holiday_supplies)
+  def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
@@ -7,6 +8,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
  holiday_hash[:winter].each do |holiday, supplies|
     supplies << supply
   end
+end
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
  holiday_hash[:spring][:memorial_day] << supply
@@ -14,17 +17,22 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
-  holiday_hashend
+  holiday_hash
+
+end
 
 def all_winter_holiday_supplies(holiday_hash)
 holiday_hash[:winter].values.flatten
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
- holidays.each do |holiday, supplies|
+
+    holidays.each do |holiday, supplies|
       puts "  #{holiday.to_s.split("_").collect{|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -34,6 +42,7 @@ holiday_hash.collect{ |season, holidays|
       supplies.include?("BBQ")
     }.keys
   }.flatten
+
 end
 
 
