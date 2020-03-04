94e85d145b11164da1555ea94a55bb4261c3d930
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-04 23:09:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,14 @@ def all_winter_holiday_supplies(holiday_hash)
 end	
 
 def all_supplies_in_holidays(holiday_hash)
-  
+  holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+
+    holidays.each do |holiday, supplies|
+      puts "  #{holiday.to_s.split("_").collect{|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
+  end
+end	
 
 
 
