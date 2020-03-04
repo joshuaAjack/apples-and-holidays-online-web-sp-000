328e27ad7488681bac488dbe7945a395676ba8e1
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-04 23:05:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,5 @@
-require 'pry'
 
-  def second_supply_for_fourth_of_july(holiday_supplies)
+ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
