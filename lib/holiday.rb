1694b1de40d4e3f064d83809935ea8dab5213146
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-04 23:10:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,3 +1,7 @@
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |holiday, supplies|
     supplies << supply
