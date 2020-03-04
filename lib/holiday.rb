fd18fe2761a3f4ef04767a9555856072e869998c
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-04 23:04:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -8,8 +8,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
  holiday_hash[:winter].each do |holiday, supplies|
     supplies << supply
   end
-end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
  holiday_hash[:spring][:memorial_day] << supply
